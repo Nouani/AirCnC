5447db761baf210b6cd18f38e3c175e78d1b759e
Nouani <nouanigabriel@hotmail.com>
2020-01-08 20:27:10 -0300

    tela de login

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,34 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Modal, SafeAreaView, AsyncStorage, Text, Image, StyleSheet } from 'react-native';
 
-export default function List(){
-    return <View />
+import logo from '../assets/logo.png';
+
+export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storageTechs => {
+            setTechs(storageTechs.trim().split(','));
+        })
+    }, [])
+
+    return (
+        <Modal >
+            <SafeAreaView style={styles.container}>
+                <Image source={logo} style={styles.logo} />
+            </SafeAreaView>
+        </Modal>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    }
+})
--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,103 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage,KeyboardAvoidingView, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Login(){
-    return <View />
+import api from '../services/api';
+import logo from '../assets/logo.png';
+
+export default function Login({ navigation }){
+    let [ email, setEmail ] = useState('');
+    let [ techs, setTechs ] = useState('');
+
+    async function handleSubmit(){
+        const response = await api.post('/sessions',{
+            email
+        })
+
+        const { _id } = response.data;
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
+    }
+
+    useEffect(()=> {
+        AsyncStorage.getItem('user').then(user => {
+            if (user){
+                navigation.navigate('List');
+            }
+        })
+    }, [])
+
+    return (
+        <KeyboardAvoidingView enabled={true} behavior="padding" style={styles.container}>
+            <Image source={logo} />
+            <View style={styles.form}>
+                <Text style={styles.label}>SEU E-MAIL *</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Seu e-mail"
+                    placeholderTextColor="#999"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                />
+                <Text style={styles.label}>TECNOLOGIAS *</Text>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Tecnologias de interesse"
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
+                />
+
+                <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                    <Text style={styles.buttonText}>Encontrar spots!</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30
+    },
+    label: {
+        fontWeight: "bold",
+        color: "#444",
+        marginBottom: 8
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+    },
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+    buttonText: {
+        color: "#FFF",
+        fontWeight: "bold",
+        fontSize: 16
+    }
+});
